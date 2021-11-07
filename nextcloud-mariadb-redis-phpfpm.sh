979094228e6da3367cacf28af343eae8431b7d6a
Michael Klamminger <6277211+m1ch@users.noreply.github.com>
2021-11-07 20:50:26 +0100

    draft

--- nextcloud-mariadb-redis-phpfpm.sh
+++ nextcloud-mariadb-redis-phpfpm.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+MARIADB_JAIL="test_mariadb"
+MARIADB_IP="10.23.10.61"
+
+NEXTCLOUD_JAIL="test_nextcloud"
+
+#install mariadb:
+iocage fetch --plugin-name "mariadb-kiss" \
+    --git_repository https://github.com/m1ch/iocage-plugin-index \
+    --branch kiss \
+    --name $MARIADB_JAIL interfaces="vnet0:bridge10" ip4_addr="vnet0|$MARIADB_IP/24" vnet=1 defaultrouter="10.23.10.1" resolver="search local;nameserver 1.1.1.1"
+
+# Create DB for mariaDB
+iocage exec $MARIADB_JAIL bash -c 'PASS=$(</root/mysqlrootpassword); mysql -u root -p"${PASS}"<<EOF
+show databases;
+DROP DATABASE  IF EXISTS testDB;
+CREATE DATABASE testDB;
+show databases;
+DROP DATABASE  IF EXISTS testDB;
+show databases;
+EOF'
+
+# install redis
+
+#install php-fpm
+
+# install nextcloud
+
+
+iocage stop $NEXTCLOUD_JAIL
+
+# Mount sockets
+iocage fstab -l nextcloud
+iocage fstab -a darkhorse /medialake2/movies /mnt/movies nullfs rw 0 0
